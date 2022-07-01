92dc042c4bab585f1a6ffec485485b959d17278f
XarthDaichi <diegoqahh@gmail.com>
2022-06-30 21:21:03 -0600

    Finishing with test and a lab template

---
+++ dataflow-binary-01/src/Product.h
@@ -0,0 +1,43 @@
+//
+// Created by Maikol Guzman Alan on 3/1/22.
+//
+
+#ifndef SOLID_01_SRP_PRODUCT_H
+#define SOLID_01_SRP_PRODUCT_H
+#include <iostream>
+#include <sstream>
+using namespace std;
+
+class Product {
+private:
+    string name;
+    string type;
+    double price;
+
+    virtual double applyDiscount();
+public:
+    Product();
+
+    virtual ~Product();
+
+    virtual double convertPrice();
+
+    const string &getName() const;
+
+    void setName(const string &name);
+
+    double getPrice() const;
+
+    void setPrice(double price);
+
+    const string &getType() const;
+
+    void setType(const string &type);
+
+    friend ostream &operator<<(ostream &os, const Product &product);
+
+    string toString() const;
+};
+
+
+#endif //SOLID_01_SRP_PRODUCT_H
